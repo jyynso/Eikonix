f2b9feca25f3b5b85c554d7e1f669fe1b9dd0c4a
jyynso <ecwt26@gmail.com>
2025-12-04 14:18:53 +0800

    working on model with db

---
+++ source/repos/WebApplication1/WebApplication1/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Data
+{
+    public class Products
+    {
+        public int product_id { get; set; }
+        public string product_title { get; set; }
+        public string product_description { get; set; }
+        public string product_size { get; set; }
+        public enum product_category { digital, traditional }
+        public string product_medium { get; set; }
+        public string product_softwareused { get; set; }
+        public string product_artist { get; set; }
+        public decimal product_price { get; set; }
+        public int product_stock { get; set; }
+        public string product_imagepath { get; set; }
+
+    }
+}
